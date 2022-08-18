354cb75825e5f281086ccc8c6d2af99dcb6e3b25
Alan Chen <alanchen@cherricorp.com>
2022-08-18 21:20:05 +0800

    Add note

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GoMail
 Send mail with Golang
+
+## 實作過程中發現的小細節
+看Go的smtp文件中有提到如果使用msg這個欄位, 需要加上"From", "To", "Subject"這些欄位在Msg中
+> The msg headers should usually include fields such as "From", "To", "Subject", and "Cc". Sending "Bcc" messages is accomplished by including an email address in the to parameter but not including it in the msg headers.
+> https://pkg.go.dev/net/smtp
+
+
+然後發現在"From", "To"這兩個欄位的後面需要加上\n才可以, 如果加上\n\n會變成之後的都會被視為Body, 所以就是在Body前的那個欄位要使用\n\n
--- sendMail.go
+++ sendMail.go
@@ -3,39 +3,27 @@ package main
 import (
 	"fmt"
 	"net/smtp"
+	"strings"
+)
+
+var (
+	from      = "go-mail@noreply.com"
+	user      = "2ac2981b8899b1"
+	password  = "5e7784e948c122"
+	host      = "smtp.mailtrap.io"
+	port      = "2525"
+	to        = []string{"john.doe@gmail.com", "joan.doe@gmail.com"}
+	subject   = "The Test Mail"
+	body      = "This is test mail by golang smtp."
+	msgString = fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\n%s",from, strings.Join(to,","), subject, body)
+	msg       = []byte(msgString)
 )
 
 func main() {
-
-	// Sender data.
-	from := "go-mail@noreply.com"
-	user := "2ac2981b8899b1"
-	password := "5e7784e948c122"
-
-	// Receiver email address.
-	to := []string{
-		"t0239184ps@gmail.com",
-	}
-
-	// smtp server configuration.
-	host := "smtp.mailtrap.io"
-	port := "2525"
-
-	// Message.
-	msg := []byte("From: john.doe@example.com\r\n" +
-		"To: roger.roe@example.com\r\n" +
-		"Subject: Test mail\r\n\r\n" +
-		"Email body\r\n")
-
-	// Authentication.
 	auth := smtp.PlainAuth("", user, password, host)
-
-	// Sending email.
-	err := smtp.SendMail(host+":"+port, auth, from, to, msg)
-
-	if err != nil {
+	if err := smtp.SendMail(host+":"+port, auth, from, to, msg); err != nil {
 		fmt.Println(err)
-		return
+	} else {
+		fmt.Println("Email Sent Successfully!")
 	}
-	fmt.Println("Email Sent Successfully!")
 }
